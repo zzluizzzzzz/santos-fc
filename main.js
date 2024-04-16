39ad4b5943a4a799a1949955edea1460ec5b3ad6
profehigor <165037678+profehigor@users.noreply.github.com>
2024-04-16 09:46:52 -0300

    Aula 16/04

--- main.js
+++ main.js
@@ -16,8 +16,32 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador")
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo2 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo3 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo4 = new Date("2024-10-05T00:00:00")
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempos[0]);
+contadores[1].textContent = calculaTempo(tempos[1]);
+contadores[2].textContent = calculaTempo(tempos[2]);
+contadores[3].textContent = calculaTempo(tempos[3]);
+
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]); } 
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000); }
+    comecaCronometro();
+
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
