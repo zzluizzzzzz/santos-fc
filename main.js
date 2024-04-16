6ae07b4690ccf6bc0102a515b5bcef18be4634d5
profehigor <165037678+profehigor@users.noreply.github.com>
2024-04-16 09:38:16 -0300

    Aula 16/04

--- main.js
+++ main.js
@@ -23,13 +23,13 @@ function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
-    let minuto = Math.floor(segundos/60);
+    let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24)
 
     segundos %=60;
     minutos %=60;
-    horas %=24%;
+    horas %=24;
 
-    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
