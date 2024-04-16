800c49dc98070038d69b6e07df5a17dc7d6854ce
profehigor <165037678+profehigor@users.noreply.github.com>
2024-04-16 09:05:58 -0300

    aula 16/04

--- main.js
+++ main.js
@@ -14,3 +14,7 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
